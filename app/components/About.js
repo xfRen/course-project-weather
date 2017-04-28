74631091ed11ee316f7f09f94fe16f403d73eed6
Xiaofei Ren <xiaofei.ren.js@gmail.com>
2017-04-28 15:19:29 -0700

    Styled About page and added links

--- app/components/About.js
+++ app/components/About.js
@@ -4,7 +4,21 @@ var About = (props) => {
   return (
     <div>
       <h1 className="text-center">About</h1>
-      <p>Place Holder</p>
+      <p>
+        This is a weather application build on React.
+        I have built this for The Complete React Web App Developer Course.
+      </p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react" target="_blank">React</a> - This was
+            the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used
+            Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   );
 };
